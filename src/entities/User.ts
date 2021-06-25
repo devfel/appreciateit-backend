c59c0442911002dd0c3d6d973235a11abff05f78
Felizardo <devfelizardo@gmail.com>
2021-06-25 03:51:40 -0300

    fixing port number

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,33 +1,31 @@
 import { Entity, PrimaryColumn, Column, CreateDateColumn, UpdateDateColumn } from "typeorm";
-import {v4 as uuid} from "uuid";
+import { v4 as uuid } from "uuid";
 
 @Entity("users")
 class User {
+  @PrimaryColumn()
+  readonly id: string;
 
-    @PrimaryColumn();
-    readonly id: string;
+  @Column()
+  name: string;
 
-    @Column();
-    name: string;
+  @Column()
+  email: string;
 
-    @Column();
-    email: string;
+  @Column()
+  admin: boolean;
 
-    @Column();
-    admin: boolean;
-    
-    @CreateDateColumn();
-    created_at: Date;
-    
-    @UpdateDateColumn();
-    updated_at: Date;
+  @CreateDateColumn()
+  created_at: Date;
 
-    constructor() {
-        if (!this.id) {
-            this.id = uuid();
-        }
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuid();
     }
-    
+  }
 }
 
 export { User };
--- src/server.ts
+++ src/server.ts
@@ -9,4 +9,4 @@ const app = express();
 app.use(express.json());
 app.use(router);
 
-app.listen(3000, () => console.log("Server up and running!"));
+app.listen(3333, () => console.log("Server up and running!"));
