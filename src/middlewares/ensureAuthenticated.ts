0c7f1ccab6167fc4dfd8d142d3b6b552cf83eb10
Felizardo <devfelizardo@gmail.com>
2021-06-28 01:34:22 -0300

    finalizing ensure adminstrator and authentication using info from database

--- src/middlewares/ensureAuthenticated.ts
+++ src/middlewares/ensureAuthenticated.ts
@@ -1,8 +1,14 @@
 import { Request, Response, NextFunction } from "express";
+import { verify } from "jsonwebtoken";
+
+interface IPayLoad {
+  sub: string;
+}
 
 export function ensureAuthenticated(request: Request, response: Response, next: NextFunction) {
   // Receive Token
-  const token = request.headers.authorization;
+  const bearerToken = request.headers.authorization;
+  const [, token] = bearerToken.split(" ");
 
   // Validate if token is not empty
   if (!token) {
@@ -10,8 +16,16 @@ export function ensureAuthenticated(request: Request, response: Response, next: 
   }
 
   // Check if token is a valid one
+  try {
+    const { sub } = verify(token, "e9b1fe4f0017aa1d72b9454c815291bf") as IPayLoad;
 
-  // Get user informations
+    // Get user informations
+    request.user_id = sub;
+
+    return next();
+  } catch (err) {
+    return response.status(401).end();
+  }
 
   return next();
 }
