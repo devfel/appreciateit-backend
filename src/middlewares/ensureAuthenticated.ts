4a65485b244856c7f8a6259efcbbca1ca1de9658
Felizardo <devfelizardo@gmail.com>
2021-06-28 02:35:21 -0300

    fix small info in create compliments controller and ensure auth middleware

--- src/middlewares/ensureAuthenticated.ts
+++ src/middlewares/ensureAuthenticated.ts
@@ -8,13 +8,13 @@ interface IPayLoad {
 export function ensureAuthenticated(request: Request, response: Response, next: NextFunction) {
   // Receive Token
   const bearerToken = request.headers.authorization;
-  const [, token] = bearerToken.split(" ");
 
-  // Validate if token is not empty
-  if (!token) {
+  // Validate if token sent is not empty
+  if (!bearerToken) {
     return response.status(401).end();
   }
 
+  const [, token] = bearerToken.split(" ");
   // Check if token is a valid one
   try {
     const { sub } = verify(token, "e9b1fe4f0017aa1d72b9454c815291bf") as IPayLoad;
